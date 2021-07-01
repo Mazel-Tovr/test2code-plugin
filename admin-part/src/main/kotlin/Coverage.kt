a159a5dba9c686d08471248fbd55b12c51c4b147
Roman <roman_ivanov3@epam.com>
2021-07-01 08:50:20 +0300

    bundle persist

--- admin-part/src/main/kotlin/Coverage.kt
+++ admin-part/src/main/kotlin/Coverage.kt
@@ -25,7 +25,6 @@ import kotlinx.collections.immutable.*
 import org.jacoco.core.data.*
 import java.io.*
 import java.util.stream.*
-import kotlin.streams.*
 
 private val logger = logger {}
 
@@ -171,8 +170,8 @@ private fun Map<String, List<Session>>.bundlesByTests(
         val testsWithEmptyBundle = flatten.testsWithBundle()
         forEach { (testType, sessions) ->
             sessions.forEach { session ->
-                if (session is FinishedSession && !session.cached.isNullOrEmpty()) {
-                    testsWithEmptyBundle.putAll(session.cached)
+                if (session is FinishedSession && !session.sessionData.bundleByTest.isNullOrEmpty()) {
+                    testsWithEmptyBundle.putAll(session.sessionData.bundleByTest)
                 } else {
                     session.asSequence()
                         .groupBy { TypedTest(it.testName, testType) }
